43dd127c8b4140a345a4cb70d2567923ec4cef07
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-06-04 21:52:19 +0200

    defy-cm7-rc0 for sync with cyanogen

---
+++ merge_cron.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Sample merge cron for subprojects
+
+git pull
+
+# git remote add cyanogen git://github.com/CyanogenMod/android.git
+#git fetch cyanogen
+#git merge remotes/cyanogen/gingerbread
+
+# git remote add defy git://github.com/CyanogenDefy/android.git
+#git fetch defy
+#git merge remotes/defy/gingerbread
+
+# git remote add tpruvot git://github.com/tpruvot/android.git
+#git fetch tpruvot
+#git merge remotes/tpruvot/gingerbread
+
+# git push
